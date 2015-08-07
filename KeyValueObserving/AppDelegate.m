c352ff676bc99c78d7c069715cc473c019a8232b
sunuo <xianyirenxunuo@gmail.com>
2015-08-07 11:08:07 +0800

    test0_[delegate.dataTableView addObserver:self forKeyPath:@"count" options:NSKeyValueObservingOptionNew context:NULL];

--- KeyValueObserving/AppDelegate.m
+++ KeyValueObserving/AppDelegate.m
@@ -13,12 +13,14 @@
 @end
 
 @implementation AppDelegate
-
-
+@synthesize dataTableView;
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    self.dataTableView=[NSMutableArray arrayWithArray:@[@"0",@"1"]];
     // Override point for customization after application launch.
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ KeyValueObserving/TVCTabelView.h
@@ -0,0 +1,13 @@
+//
+//  TVCTabelView.h
+//  KeyValueObserving
+//
+//  Created by kevin on 15/8/7.
+//  Copyright (c) 2015年 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVCTabelView : UITableViewController
+
+@end
